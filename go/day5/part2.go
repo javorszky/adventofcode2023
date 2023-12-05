8de40e3b8f5f53b31a473ecd5a3098d66777e5fc
Gabor Javorszky <gabor@javorszky.co.uk>
2023-12-05 15:32:14 +0000

    Day 5 part 2 does not complete in time solution

--- go/day5/part2.go
+++ go/day5/part2.go
@@ -2,6 +2,7 @@ package day5
 
 import (
 	"os"
+	"sort"
 
 	"github.com/javorszky/adventofcode2023/inputs"
 	"github.com/rs/zerolog"
@@ -10,15 +11,35 @@ import (
 func Task2(l zerolog.Logger) {
 	localLogger := l.With().Int("day", 5).Int("part", 2).Logger()
 
-	_, err := inputs.ReadIntoLines("day5/input2.txt")
+	groups, err := inputs.GroupByBlankLines("day5/input1.txt")
 	if err != nil {
 		localLogger.Err(err).Msg("could not read input file")
 		os.Exit(1)
 	}
 
-	// code goes here
+	m, err := parseGroups(groups)
+	if err != nil {
+		localLogger.Err(err).Msg("parseGroups returned an error")
+	}
 
-	solution := 2
+	// extract the seeds first
+	seeds, err := parseSeeds(groups[0][0])
+	if err != nil {
+		localLogger.Err(err).Msgf("parseSeeds")
+	}
+
+	locations := make([]int, 0)
+
+	for i := 0; i < len(seeds); i = i + 2 {
+		for j := 0; j < seeds[i+1]; j++ {
+			locations = append(locations, walkSeed(m, seeds[i]+j))
+		}
+	}
+
+	sort.Ints(locations)
+	solution := locations[0]
 	s := localLogger.With().Int("solution", solution).Logger()
-	s.Info().Msgf("-- change this to the part 2 message -- %d", solution)
+	s.Info().Msgf("Lowest location when the seeds are ranges is %d", solution)
 }
+
+// destination - source - length
