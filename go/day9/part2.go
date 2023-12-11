131435cbea8d991554694e459533b5713cbdd3e9
Gabor Javorszky <gabor@javorszky.co.uk>
2023-12-11 16:24:03 +0000

    Day 9 part 2 solved

--- go/day9/part2.go
+++ go/day9/part2.go
@@ -1,6 +1,7 @@
 package day9
 
 import (
+	"github.com/pkg/errors"
 	"os"
 
 	"github.com/javorszky/adventofcode2023/inputs"
@@ -10,15 +11,78 @@ import (
 func Task2(l zerolog.Logger) {
 	localLogger := l.With().Int("day", 9).Int("part", 2).Logger()
 
-	_, err := inputs.ReadIntoLines("day9/input2.txt")
+	gog, err := inputs.ReadIntoLines("day9/input1.txt")
 	if err != nil {
 		localLogger.Err(err).Msg("could not read input file")
 		os.Exit(1)
 	}
 
-	// code goes here
+	lines := make([][]int, len(gog))
 
-	solution := 2
+	for i, line := range gog {
+		numbers, err := parseIntoInts(line)
+		if err != nil {
+			localLogger.Err(err).Msgf("Turning line '%s' into ints died", line)
+		}
+
+		lines[i] = numbers
+	}
+
+	sum := 0
+	//p, err := predictFront(lines[2])
+	//if err != nil {
+	//	localLogger.Err(err).Msgf("trying this line: %v, the predict error happened", lines[0])
+	//}
+	//
+	//fmt.Printf("prediction is %d\n", p)
+
+	for _, line := range lines {
+		p, err := predictFront(line)
+		if err != nil {
+			localLogger.Err(err).Msgf("trying this line: %v, the predict error happened", lines[0])
+		}
+
+		sum += p
+	}
+
+	solution := sum
 	s := localLogger.With().Int("solution", solution).Logger()
-	s.Info().Msgf("-- change this to the part 2 message -- %d", solution)
+	s.Info().Msgf("Predicting backwards yields %d", solution)
+}
+
+func predictFront(in []int) (int, error) {
+	//fmt.Printf("======== start ========\n"+
+	//	"predicting next for %v\n", in)
+
+	diffs, err := getDifferences(in)
+	if err != nil {
+		return 0, errors.Wrapf(err, "getDifferences for slice %v", in)
+	}
+
+	allZero := true
+	for _, v := range diffs {
+		if v != 0 {
+			//fmt.Printf("diffs not all zero (%v)\n", diffs)
+			allZero = false
+			break
+		}
+	}
+
+	delta := 0
+
+	if !allZero {
+		d, err := predictFront(diffs)
+		if err != nil {
+			return 0, errors.Wrapf(err, "predict returned error for this slice: %v", diffs)
+		}
+
+		//fmt.Printf("predicted %d for the diffs\n", d)
+
+		delta = d
+	} else {
+		//fmt.Printf("diffs all zero: %v\n", diffs)
+	}
+
+	//fmt.Printf("returning %d as the prediction\n", in[0]-delta)
+	return in[0] - delta, nil
 }
